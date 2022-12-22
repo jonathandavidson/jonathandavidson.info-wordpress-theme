77e92cb1baf33303d4d3f5caeb1d82b773662d25
Jonathan Davidson <jdavidson@netsplasher.com>
2022-12-21 23:43:22 -0600

    Adds logos

--- patterns/cloud-banner.php
+++ patterns/cloud-banner.php
@@ -21,7 +21,31 @@
           <div class="jonathandavidson-info--cloud-banner--cloud-layer">
             <!-- wp:group {"className":"jonathandavidson-info--cloud-banner--cloud-layer-inner","layout":{"type":"flex"}} -->
             <div class="jonathandavidson-info--cloud-banner--cloud-layer-inner">
-              jello world!
+              <ul>
+                <li>
+                  <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/node-logo.png' ) ); ?>" alt="NodeJS" class="wp-image-170" width="170"/>
+                </li>
+                <li>
+                  <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/python-logo.png' ) ); ?>" alt="Python" class="wp-image-214" width="214"/>
+                </li>
+                <li>
+                  <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/java-logo.png' ) ); ?>" alt="Java" class="wp-image-170" width="170"/>
+                </li>              
+              </ul>
+              <ul>
+                <li>
+                  <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/react-logo.png' ) ); ?>" alt="React" class="wp-image-187" width="187"/>
+                </li>
+                <li>
+                  <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/angular-logo.png' ) ); ?>" alt="Angular" class="wp-image-200" width="200"/>
+                </li>
+                <li>
+                  <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/html-logo.png' ) ); ?>" alt="HTML" class="wp-image-68" width="68"/>
+                </li>
+                <li>
+                  <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/css-logo.png' ) ); ?>" alt="CSS" class="wp-image-68" width="68"/>
+                </li>
+              </ul>
             </div>
             <!-- /wp:group -->
           </div>
