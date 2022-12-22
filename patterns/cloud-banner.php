b1ed95c8e3e0d8cd098a3d9fe8ef20e33b7900d8
Jonathan Davidson <jdavidson@netsplasher.com>
2022-12-22 11:48:43 -0600

    Styling of cloud banner content

--- patterns/cloud-banner.php
+++ patterns/cloud-banner.php
@@ -21,31 +21,35 @@
           <div class="jonathandavidson-info--cloud-banner--cloud-layer">
             <!-- wp:group {"className":"jonathandavidson-info--cloud-banner--cloud-layer-inner","layout":{"type":"flex"}} -->
             <div class="jonathandavidson-info--cloud-banner--cloud-layer-inner">
-              <ul>
-                <li>
-                  <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/node-logo.png' ) ); ?>" alt="NodeJS" class="wp-image-170" width="170"/>
-                </li>
-                <li>
-                  <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/python-logo.png' ) ); ?>" alt="Python" class="wp-image-214" width="214"/>
-                </li>
-                <li>
-                  <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/java-logo.png' ) ); ?>" alt="Java" class="wp-image-170" width="170"/>
-                </li>              
-              </ul>
-              <ul>
-                <li>
-                  <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/react-logo.png' ) ); ?>" alt="React" class="wp-image-187" width="187"/>
-                </li>
-                <li>
-                  <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/angular-logo.png' ) ); ?>" alt="Angular" class="wp-image-200" width="200"/>
-                </li>
-                <li>
-                  <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/html-logo.png' ) ); ?>" alt="HTML" class="wp-image-68" width="68"/>
-                </li>
-                <li>
-                  <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/css-logo.png' ) ); ?>" alt="CSS" class="wp-image-68" width="68"/>
-                </li>
-              </ul>
+              <div>
+                <h3>Backend</h3>
+                <ul>
+                  <li>
+                    <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/node-logo.png' ) ); ?>" alt="NodeJS" class="wp-image-170" width="170"/>
+                  </li>
+                  <li>
+                    <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/python-logo.png' ) ); ?>" alt="Python" class="wp-image-214" width="214"/>
+                  </li>
+                  <li>
+                    <img style="position: relative; margin-top: -2em;" src="<?php echo esc_url( get_theme_file_uri( 'assets/images/java-logo.png' ) ); ?>" alt="Java" class="wp-image-170" width="170"/>
+                  </li>              
+                </ul>
+              </div>
+              <div>
+                <h3>Frontend</h3>
+                <ul>
+                  <li>
+                    <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/react-logo.png' ) ); ?>" alt="React" class="wp-image-187" width="187"/>
+                  </li>
+                  <li>
+                    <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/angular-logo.png' ) ); ?>" alt="Angular" class="wp-image-200" width="200"/>
+                  </li>
+                  <li>
+                    <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/html-logo.png' ) ); ?>" alt="HTML" class="wp-image-68" width="68"/>
+                    <img src="<?php echo esc_url( get_theme_file_uri( 'assets/images/css-logo.png' ) ); ?>" alt="CSS" class="wp-image-68" width="68"/>
+                  </li>
+                </ul>
+              </div>
             </div>
             <!-- /wp:group -->
           </div>
