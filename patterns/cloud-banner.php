e35848a553ae4b3eb928433db389c82c28509663
Jonathan Davidson <jdavidson@netsplasher.com>
2022-12-21 18:53:45 -0600

    Adds cloud-banner pattern

--- patterns/cloud-banner.php
+++ patterns/cloud-banner.php
@@ -3,13 +3,21 @@
  * Title: Cloud Banner
  * Slug: twentytwentythree/cloud-banner
  * Categories: featured
- * Keywords: bloud banner
+ * Keywords: cloud banner
  * Block Types: core/buttons
  */
 ?>
 <!-- wp:group {"className":"jonathandavidson-info--cloud-banner","layout":{"type":"flex"}} -->
 <div class="wp-block-group jonathandavidson-info--cloud-banner">
-  <div class="jonathandavidson-info--cloud-banner--cloud-layer"></div>
+  <!-- wp:group {"className":"jonathandavidson-info--cloud-banner--cloud-layer","layout":{"type":"flex"}} -->
+  <div class="jonathandavidson-info--cloud-banner--cloud-layer">
+    <!-- wp:group {"className":"jonathandavidson-info--cloud-banner--cloud-layer","layout":{"type":"flex"}} -->
+    <div class="jonathandavidson-info--cloud-banner--cloud-layer-inner">
+      jello world!
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
 </div>
 <!-- /wp:group -->
 
